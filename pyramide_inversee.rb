d422cea5262066b8024c87619996e9b0f09f5b53
admin <admin@pc-7.home>
2019-03-28 20:11:26 +0100

    ci-joint les exercices ruby Jour 1 & 2

---
+++ pyramide_inversee.rb
@@ -0,0 +1,13 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu entre 1 et 25?"
+print "> "
+steps = gets.to_i
+
+if steps <= 0
+	puts "nombre trop petit"
+elsif  steps > 25
+	puts "nombre trop grand"
+else
+	for i in (1..steps)
+		puts (" "*(steps-i)) + ("#"*i)
+	end
+end
